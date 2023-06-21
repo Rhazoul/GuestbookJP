2912e1b6a62311b40ddda0c36e98970cf78ed110
Rhazoul <jperoche@gmail.com>
2023-06-21 09:04:46 +0200

    13.2 v2

--- migrations/Version20230621065320.php
+++ migrations/Version20230621065320.php
@@ -20,14 +20,14 @@ final class Version20230621065320 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment CHANGE id id VARCHAR(255) NOT NULL, CHANGE conference_id conference_id VARCHAR(255) NOT NULL');
+        
         $this->addSql('ALTER TABLE conference ADD slug VARCHAR(255) NOT NULL, CHANGE id id VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE conference_id conference_id INT NOT NULL');
+        
         $this->addSql('ALTER TABLE conference DROP slug, CHANGE id id INT AUTO_INCREMENT NOT NULL');
     }
 }
